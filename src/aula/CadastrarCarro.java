93efaf36221d3482ecc038f0a61c3793de287be9
Wesley vieira de lima <wesley.vieira.lima.1999@gmail.com>
2017-08-08 22:13:39 -0300

    Update CadastrarCarro.java

--- src/aula/CadastrarCarro.java
+++ src/aula/CadastrarCarro.java
@@ -3,10 +3,18 @@ package aula;
 public class CadastrarCarro {
 	public static void main(String args[]) {
 		Carro carro = new Carro();
+		
 		carro.modelo = "uno";
 		carro.ano = 1994;
 		carro.cor = "preto";
 
+		Carro carro_um = new Carro();
+		
+		carro_um.modelo = "Palio";
+		carro_um.ano = 2007;
+		carro_um.cor = "Vermelhor";
+		
 		System.out.println("Modelo: " + carro.modelo);
+		
 	}
 }
